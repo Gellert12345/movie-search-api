03e52bcf00b2f87724266ef4b86cda4d94d4060c
Kecskés Gellért <gellertkecskes@gmail.com>
2026-01-31 13:05:50 +0100

    Add files via upload

--- script33.js
+++ script33.js
@@ -16,8 +16,17 @@ document.getElementById("search").onsubmit = async function(event) {
     }
     //json tartalom kiszedés
     var movieResponse = await response.json();
+    //hiba kezelő rendszer:
+
+    if(!movieResponse.Search) {
+        alert("Keresés sikertelen");
+        return
+    }
+    renderMovieList(movieResponse.Search);
+}
+function renderMovieList(movies) {
     var moveListTemplate = ""; //inizializálás
-    for(var movie of movieResponse.Search) {
+    for(var movie of movies) {
         moveListTemplate = moveListTemplate +  `
         <li>
         <div class="poster-wrap">
@@ -25,8 +34,8 @@ document.getElementById("search").onsubmit = async function(event) {
                 <img src="${movie.Poster}" class="movie-poster"/>
             </a>
             </div>
-            <p data-imbid="${movie.imdbID}" class="single-movie-btn">
-                <span class="movie-title">
+            <p class="single-movie-btn">
+                <span class="movie-title" data-imbid="${movie.imdbID}">
                     ${movie.Title}
                 </span>
             </p>
@@ -38,4 +47,20 @@ document.getElementById("search").onsubmit = async function(event) {
         `
     }
     document.getElementById("movies").innerHTML = moveListTemplate
+    var movieTitles = document.querySelectorAll(".single-movie-btn")
+    for(var movieTitle of movieTitles) {
+        movieTitle.onclick = function(event) {
+            var url = `https://www.omdbapi.com/?i=${event.target.parentElement.dataset.imdbID}&apiKey=9606ae0f`
+            fetch(url) //promise lesz belőle
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(singleMovie){ // singleMovie=== result
+                document.getElementById("movie-description").innerHTML = ` //log-ba van leirva az api kulcs adatinak az azonsítója pl title,director stb
+                <h1>${singleMovie.Title}</h1> 
+                <p>${singleMovie.Plot}</p>
+                `
+            })
+        }
+    }
 }
